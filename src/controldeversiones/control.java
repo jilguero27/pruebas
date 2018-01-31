ac7facff2a4ea1e304947728c3409b68d215a908
dam <dam@203P01>
2018-01-31 13:34:58 +0100

    Primera version

---
+++ src/controldeversiones/control.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controldeversiones;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author dam
+ */
+public class control {
+    
+    public static void main(String[] args) throws IOException {
+    
+    }
+}
